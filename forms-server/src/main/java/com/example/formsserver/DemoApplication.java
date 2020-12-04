0d710984805f1c37d3525e3872d8934f3044886f
Christian Kaufhold <christian.kaufhold@bsb-muenchen.de>
2020-12-04 09:39:55 +0100

    initial version of forms-server

---
+++ forms-server/src/main/java/com/example/formsserver/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.formsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// generiert mit https://start.spring.io/
+@SpringBootApplication
+public class DemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
+}
