fa258ac1a8e8059a128d78f8e84af421aad57f3b
Christian Kaufhold <christian.kaufhold@bsb-muenchen.de>
2021-04-29 17:09:58 +0200

    get all props of an document

--- document-server/src/main/java/com/example/docuserver/dto/DtoService.java
+++ document-server/src/main/java/com/example/docuserver/dto/DtoService.java
@@ -2,25 +2,52 @@ package com.example.docuserver.dto;
 
 import com.example.cmisdemo.model.*;
 import com.example.docuserver.model.CmisDoc;
+import example.com.cmisservices.repository.TypeDefinitionRepository;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DtoService {
+
+  @Autowired TypeDefinitionRepository typeDefinitionRepository;
 
   public List<Document> cmisDocsToDocuments(List<CmisDoc> cmisDocs) {
     return cmisDocs.stream().map(this::cmisDocToDocument).collect(Collectors.toList());
   }
 
   public Document cmisDocToDocument(CmisDoc cmisDoc) {
-    // todo get type
-    PropertyDefinition iddef =
-        new PropertyDefinition("id", "id", Constants.OBJECTTYPE_STRING, Cardinality.SINGLE);
-
     Document document = new Document();
-    document.getProperties().add(new CmisProperty(iddef, cmisDoc.getId()));
-    return document;
+    document
+        .getProperties()
+        .add(
+            new CmisProperty(
+                new PropertyDefinition("id", "id", Constants.OBJECTTYPE_STRING, Cardinality.SINGLE),
+                cmisDoc.getId()));
+    document
+        .getProperties()
+        .add(
+            new CmisProperty(
+                new PropertyDefinition(
+                    "typeId", "typeId", Constants.OBJECTTYPE_STRING, Cardinality.SINGLE),
+                cmisDoc.getTypeId()));
+
+    try {
+      ObjectType objectType = typeDefinitionRepository.getTypeDefinition(cmisDoc.getTypeId());
+      for (PropertyDefinition pd : objectType.getPropertyDefinitions()) {
+        PropertyDefinition propDef =
+            new PropertyDefinition(
+                pd.getPropertyId(), pd.getDisplayName(), pd.getTypeId(), pd.getCardinality());
+        document
+            .getProperties()
+            .add(new CmisProperty(propDef, cmisDoc.getTextMap().get(pd.getPropertyId() + "_t")));
+      }
+
+      return document;
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
   }
 
   public CmisDoc documentToCmisDoc(Document document) {
